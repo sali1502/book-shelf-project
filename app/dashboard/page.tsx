d3bf4f53dac5fe691ba2b738813b17e9fb4e219a
sali1502 <sali1502@sudent.miun.se>
2026-03-04 15:58:54 +0100

    Add layout to header closes #1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ const title = "Dashboard";
 
 export default function Dashboard() {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto px-4">
             <article>
                 <h1>{title}</h1>
             </article>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+/* Landing page */
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Landing page",
+    description: "Landing page with books",
+};
+
+const title = "Landing page";
 
 export default function Home() {
   return (
-    <main>
-      <p>Landing page</p>
-    </main>
+      <div className="max-w-7xl mx-auto px-4">
+            <article>
+                <h1>{title}</h1>
+            </article>
+        </div>
   );
 }
